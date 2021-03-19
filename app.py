5123d9d27cd8d8d45a2b8ad91edda018771c0c7e
qjatjr29 <74031333+qjatjr29@users.noreply.github.com>
2021-03-19 20:14:04 +0900

    add BJ,Algo DB

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from algospot import getAlgorithmName
 # app = Flask("AlgorithmSearch")
 app = Flask(__name__)
 
-db = {}
+BJdb = {}
+Algodb = {}
 
 
 @app.route("/")
@@ -16,42 +17,45 @@ def homepage():
 
 @app.route("/baekjoon")
 def baekjoon():
-    print("2")
     Algorithm__tag = request.args.get("Algorithm__baekjoon")
     if Algorithm__tag:
         Algorithm__tag = Algorithm__tag.lower()
         # Tag=getAlgorithmTag(Algorithm__tag)
-        existingTag = db.get(Algorithm__tag)
+        existingTag = BJdb.get(Algorithm__tag)
+        print(existingTag)
         if existingTag:
             problems = existingTag
         else:
             problems = getAlgorithmTag(Algorithm__tag)
-            db[Algorithm__tag] = problems
+            BJdb[Algorithm__tag] = problems
     else:
         return redirect("/")
-    return render_template("baekjoon.html", searchingBy=Algorithm__tag, resultsNumber=len(problems), problems=problems)
+    return render_template("baekjoon.html",
+                           searchingBy=Algorithm__tag, resultsNumber=len(
+                               problems),
+                           problems=problems)
 
 
 @app.route("/algospot")
 def algospot():
-    # print("3")
     Algorithm__tag = request.args.get("Algorithm__algospot")
     if Algorithm__tag:
         Algorithm__tag = Algorithm__tag.lower()
         # Tag=getAlgorithmTag(Algorithm__tag)
-        existingTag = db.get(Algorithm__tag)
+        existingTag = Algodb.get(Algorithm__tag)
+        print(existingTag)
         if existingTag:
             problems = existingTag
         else:
             problems = getAlgorithmName(Algorithm__tag)
-            db[Algorithm__tag] = problems
+            Algodb[Algorithm__tag] = problems
     else:
         return redirect("/")
-    return render_template("algospot.html", searchingBy=Algorithm__tag, resultsNumber=len(problems), problems=problems)
+    return render_template("algospot.html", searchingBy=Algorithm__tag,
+                           resultsNumber=len(problems), problems=problems)
 
 
 if __name__ == "__main__":
-    print("1")
     app.run()
     # app.run(host='0.0.0.0', port="5000", debug=True)
 
