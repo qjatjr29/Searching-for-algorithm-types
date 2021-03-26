67ec1d0e0cdc3446315a892588e04df484637323
qjatjr29 <74031333+qjatjr29@users.noreply.github.com>
2021-03-26 20:47:27 +0900

    main-img update and groupsearch result page design

---
+++ static/JavaScript/background.js
@@ -0,0 +1,19 @@
+const mainimg = document.querySelector(".main_img");
+const IMG_NUM = 4;
+
+function displayImage() {
+    const ImageNumber = Math.floor(Math.random() * IMG_NUM);
+    const background_img = new Image();
+    // background_img.src = `../static/img/${ImageNumber + 1}.jpg`;
+    background_img.src = `{{ url_for('static', filename = img/${ImageNumber + 1}.jpg) }}`;
+    background_img.classList.add("backgroundIMG");
+    mainimg.appendChild(background_img);
+}
+
+
+
+function init() {
+    displayImage();
+}
+
+init();
