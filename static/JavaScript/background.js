e3b5b01219215cc7812fa28b69e9788443f6066f
qjatjr29 <74031333+qjatjr29@users.noreply.github.com>
2021-03-27 20:45:27 +0900

    main img is done,todolist design

--- static/JavaScript/background.js
+++ static/JavaScript/background.js
@@ -5,7 +5,8 @@ function displayImage() {
     const ImageNumber = Math.floor(Math.random() * IMG_NUM);
     const background_img = new Image();
     // background_img.src = `../static/img/${ImageNumber + 1}.jpg`;
-    background_img.src = `{{ url_for('static', filename = img/${ImageNumber + 1}.jpg) }}`;
+    // background_img.src = "{{ url_for('static',filename = img/" + `${ImageNumber + 1}` + ".jpg) }}";
+    background_img.src = `../static/img/${ImageNumber + 1}.jpg`;
     background_img.classList.add("backgroundIMG");
     mainimg.appendChild(background_img);
 }
